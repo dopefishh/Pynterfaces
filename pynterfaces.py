47017de7fff42e51047d8314aa9fb4b1f9d0aa11
Mart Lubbers <mart@martlubbers.net>
2014-04-01 16:08:50 +0200

    version 0.07 pep8 fix

--- pynterfaces.py
+++ pynterfaces.py
@@ -6,80 +6,121 @@ import sys
 import wizards
 from interfaces import interfaces
 
+
 def mainparser(args):
-	parser = argparse.ArgumentParser(description="Edit /etc/network/interfaces via CLI\n Help for every command can be accessed by -h after the command")
-	parser.add_argument("command", 
-			action="store", 
-			help="Command", 
-			choices=["list", "add", "rm"], 
-			type=str)
-	parser.add_argument("-v", "--verbose",
-			action="store_true",
-			help="More verbose output")
-	parser.add_argument("--input", 
-			action="store", 
-			default="/etc/network/interfaces",
-			metavar="PATH",
-			help="Use an alternate input file instead of /etc/network/interfaces, - for stdin",
-			type=str)
-	parser.add_argument("--output",
-			action="store",
-			default="/etc/network/interfaces",
-			metavar="PATH",
-			help="Use an alternate output file instead of /etc/network/interfaces, - for stdout, when exists backup is created.",
-			type=str)
-	return parser.parse_args(args)
+    parser = argparse.ArgumentParser(
+        description='Edit /etc/network/interfaces via CLI\n\
+            Help for every command can be accessed by -h after the command'
+        )
+    parser.add_argument(
+        'command',
+        action='store',
+        help='Command',
+        choices=['list', 'add', 'rm'],
+        type=str
+        )
+    parser.add_argument(
+        '-v', '--verbose',
+        action='store_true',
+        help='More verbose output'
+        )
+    parser.add_argument(
+        '--input',
+        action='store',
+        default='/etc/network/interfaces',
+        metavar='PATH',
+        help='Use an alternate input file instead of /etc/network/interfaces,\
+              - for stdin',
+        type=str
+        )
+    parser.add_argument(
+        '--output',
+        action='store',
+        default='/etc/network/interfaces',
+        metavar='PATH',
+        help='Use an alternate output file instead of /etc/network/interfaces,\
+              - for stdout, when exists backup is created.',
+        type=str
+        )
+    return parser.parse_args(args)
+
 
 def secondaryparser(args, opttype):
-	if opttype == "rm":
-		parser = argparse.ArgumentParser(description="Help file for rm command", 
-				usage="%(prog)s [OPTIONS] rm [-h] name")
-		parser.add_argument("name",
-				action="store",
-				help="Removes the interface given")
-	elif opttype == "list":
-		parser = argparse.ArgumentParser(description="Help file for list command",
-				usage="%(prog)s [OPTIONS] list [-h]")
-	else:
-		parser = argparse.ArgumentParser(description="Help file for add command",
-				usage="%(prog)s [OPTIONS] add [-h] {loopback, device, network} [key=value]")
-		parser.add_argument("--scriptpath",
-				action="store",
-				metavar="PATH",
-				help="Script location to store the mapping scripts, default: /etc/network/mapping\nPlease use a full path and no ~")
-		parser.add_argument("type",
-				action="store",
-				help="Enter the type of interface/defice you want to add",
-				choices=["loopback", "device", "network"],
-				)
-		parser.add_argument("kwargs",
-				action="store",
-				help="Prefill questions in the format: key1=value1,key2=value2...keyn=valuen",
-				nargs="?")
-	return parser.parse_args(args)
+    if opttype == 'rm':
+        parser = argparse.ArgumentParser(
+            description='Help file for rm command',
+            usage='%(prog)s [OPTIONS] rm [-h] name'
+            )
+        parser.add_argument(
+            '--regex', '-r',
+            action='store_true',
+            help='Use python regexes and remove all matching interfaces'
+            )
+        parser.add_argument(
+            'name',
+            action='store',
+            help='Removes the interface given'
+            )
+    elif opttype == 'list':
+        parser = argparse.ArgumentParser(
+            description='Help file for list command',
+            usage='%(prog)s [OPTIONS] list [-h]'
+            )
+    else:
+        parser = argparse.ArgumentParser(
+            description='Help file for add command',
+            usage='%(prog)s [OPTIONS] add [-h] \
+                {loopback, device, network} [key=value]'
+            )
+        parser.add_argument(
+            '--scriptpath',
+            action='store',
+            metavar='PATH',
+            help='Script location to store the mapping scripts, default: \
+                  /etc/network/mapping\nPlease use a full path and no ~'
+            )
+        parser.add_argument(
+            'type',
+            action='store',
+            help='Enter the type of interface/defice you want to add',
+            choices=['loopback', 'device', 'network'],
+            )
+        parser.add_argument(
+            'kwargs',
+            action='store',
+            help='Args: key1=value1,key2=value2...keyn=valuen',
+            nargs='?')
+    return parser.parse_args(args)
 
-if __name__ == "__main__":
-	index = sys.argv.index("rm") if "rm" in sys.argv else sys.argv.index("list") if "list" in sys.argv else sys.argv.index("add") if "add" in sys.argv else len(sys.argv)
-	ms = mainparser(sys.argv[1:(index+1)])
-	os = secondaryparser(sys.argv[index+1:], ms.command)
+if __name__ == '__main__':
+    index = sys.argv.index('rm') if 'rm' in sys.argv else\
+        sys.argv.index('list') if 'list' in sys.argv else\
+        sys.argv.index('add') if 'add' in sys.argv else len(sys.argv)
+    ms = mainparser(sys.argv[1:(index+1)])
+    os = secondaryparser(sys.argv[index+1:], ms.command)
 
-	intfile = interfaces(ms.input)
+    intfile = interfaces(ms.input)
 
-	if ms.command == "list":
-		if ms.verbose: print "List command applied"
-		print intfile
-	elif ms.command == "add":
-		if ms.verbose: print "Add command applied"
-		kw = dict() if not os.kwargs else dict(i.split('=') for i in os.kwargs.split(','))
-		if ms.verbose: print "kwargs parsed: %s" % str(kw)
-		if os.type == "loopback":
-			wizards.addloopback(intfile, ms.verbose, **kw)
-		if os.type == "device":
-			wizards.adddevice(intfile, os.scriptpath if os.scriptpath else "/etc/network/mapping", ms.verbose, **kw)
-		if os.type == "network":
-			wizards.addnetwork(intfile, ms.verbose, **kw)
-	elif ms.command == "rm":
-		if ms.verbose: print "Remove command applied"
-		intfile.remove(os.name, ms.verbose)
-	
-	intfile.tofile(ms.output)
+    if ms.command == 'list':
+        if ms.verbose:
+            print 'List command applied'
+        print intfile
+    elif ms.command == 'add':
+        if ms.verbose:
+            print 'Add command applied'
+        kw = dict() if not os.kwargs else\
+            dict(i.split('=') for i in os.kwargs.split(','))
+        if ms.verbose:
+            print 'kwargs parsed: %s' % str(kw)
+        if os.type == 'loopback':
+            wizards.addloopback(intfile, ms.verbose, **kw)
+        if os.type == 'device':
+            wizards.adddevice(intfile, os.scriptpath if os.scriptpath else
+                              '/etc/network/mapping', ms.verbose, **kw)
+        if os.type == 'network':
+            wizards.addnetwork(intfile, ms.verbose, **kw)
+    elif ms.command == 'rm':
+        if ms.verbose:
+            print 'Remove command applied'
+        intfile.remove(os.name, os.regex, ms.verbose)
+    intfile.tofile(ms.output)
--- scripts.py
+++ scripts.py
@@ -1,7 +1,10 @@
 # -*- coding: utf-8 -*-
 
-s = {"ssid": ("Pick the script according to the ssid, logical interface will be called as the ssid",
-"""#!/bin/sh
-ifconfig %(interface)s up 1> /dev/null && { iwlist %(interface)s scan | grep -o "\".*\"" | tr -d \'\" | sort | uniq; grep -o "map\ .*" /etc/network/interfaces | awk '{print $2}'; } | sort | uniq -d | head -1
-""")
-}
+s = {
+    "ssid": ("Pick the script according to the ssid, logical interface will be\
+called as the ssid, configfiles should be named ssid.conf",
+             """#!/bin/sh
+ifconfig %(interface)s up 1> /dev/null && { iwlist %(interface)s scan | grep \
+-o "\".*\"" | tr -d \'\" | sort | uniq; grep -o "map\ .*" /etc/network/interfa\
+ces | awk '{print $2}'; } | sort | uniq -d | head -1""")
+    }
